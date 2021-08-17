cb4b474a0c32349f1b6e1ffdf8220ed1db858a5d
Sheldon H Johnson <johnsonsheldon332@gmail.com>
2021-08-17 09:35:30 -0400

    Co-authored-by: Tyler Holt <TylersToybox@users.noreply.github.com> Co-authored-by: jespin457 <jespin457@users.noreply.github.com>

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,16 +1,17 @@
 import React, {useState, useEffect, useReducer, useContext} from 'react';
 import { AppContext } from "./App.jsx";
 import {Link} from 'react-router-dom';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 const Nav = () =>{
 
     return (
 <nav className="nav-container">
    <div className="logo">
-<Link to="/">logo</Link>
+<Link to="/"><img src="../../images/logocook.png" id="cooklogo"/></Link>
 
    </div>
    <div className="interactables">
-      <Link to="/cart">Cart</Link>
+      <Link to="/cart"><ShoppingCartIcon/></Link>
    <br></br>  <Link to="/signin">Login</Link>
       
    </div>
